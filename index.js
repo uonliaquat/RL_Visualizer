0da28737f830a385233f7dadd4a199ab3ece9b65
Uon Liaquat <antelopes06@gmail.com>
2021-02-18 01:06:21 +0500

    Set Constraint for width and heigh

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var height = 0;
 generate_btn.addEventListener("click", () => {
 		width =  document.getElementById('cols').value;
 		height =  document.getElementById('rows').value;
-		console.log(width);
-		console.log(height);
+		if(width > 20 || height > 20) return alert('Width and Height must be less than or equal to 20'); 
     window.location.href = `/GridWorld/index.html?width=${width}&height=${height}`;
 });
