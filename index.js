594500d4461f85e2904733f3edc5e033b7a94954
Uon Liaquat <antelopes06@gmail.com>
2021-02-28 13:01:24 +0500

    Updated index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var height = 0;
 generate_btn.addEventListener("click", () => {
 		width =  document.getElementById('cols').value;
 		height =  document.getElementById('rows').value;
-    window.location.href = `RL_Visualizer/GridWorld/index.html?width=${width}&height=${height}`;
+    window.location.href = `/RL_Visualizer/GridWorld/index.html?width=${width}&height=${height}`;
 });
